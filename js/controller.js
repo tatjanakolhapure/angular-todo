c6de3a7b55ea6ebcd40829f0ccbb2659a923b9df
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-02-26 18:48:26 +0000

    Add Service controller

--- js/controller.js
+++ js/controller.js
@@ -2,16 +2,23 @@ angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
         $scope.title = "Welcome To Angular Todo!";
     })
-    .controller('RegisterController', function($scope) {
+    .controller('RegisterController', function($scope, $location, UserAPIService) {
  
         $scope.registrationUser = {};
+        var URL = "https://morning-castle-91468.herokuapp.com/";
  
         $scope.submitForm = function() {
             if ($scope.registrationForm.$valid) {
                 $scope.registrationUser.username = $scope.user.username;
                 $scope.registrationUser.password = $scope.user.password;
-            }
  
-            console.log($scope.registrationUser.username + " " + $scope.registrationUser.password);
+                UserAPIService.registerUser(URL + "accounts/register/", $scope.registrationUser).then(function(results) {
+                    $scope.data = results.data;
+                    alert("You have successfully registered to Angular Todo");
+                }).catch(function(err) {
+                    alert("Oops! Something went wrong!");
+                    console.log(err);
+                });
+            }
         };
     });
