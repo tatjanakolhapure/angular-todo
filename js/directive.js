3c2f960e312180de94492966e55f5a56856305cf
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-03-13 23:07:56 +0000

    Add navbar directive

--- js/directive.js
+++ js/directive.js
@@ -1,6 +1,15 @@
-angular.module('TodoDirective',[]).directive('todoTable', function() {
+angular.module('TodoDirective',[])
+
+.directive('todoTable', function() {
   return {
     restrict: 'E',    //  E -> element
     templateUrl: 'templates/todo-table.html'
   };
+})
+
+.directive('navbar', function (){
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/navbar.html'
+	};
 });
